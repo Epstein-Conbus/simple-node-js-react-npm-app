268c992985c8a7cbfadf2866f21b0935d0bffd23
Jenkins <nobody@nowhere>
2024-05-31 09:43:03 +0000

    젠킨스 빌드 및 배포가 완료되었습니다.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
 
   render() {
-      console.log(1717147970077)
+      console.log(1717148570603)
       console.log('hi')
     return (
       <div className="App">
